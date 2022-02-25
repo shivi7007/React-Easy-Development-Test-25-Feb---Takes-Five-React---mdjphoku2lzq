29f5b898e9ef76aadf4ce2fa685d8aedbf2ca2f0
shivi7007 <93267823+shivi7007@users.noreply.github.com>
2022-02-25 19:25:57 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -2,14 +2,22 @@ import React from 'react'
 
 import '../styles/App.css';
 const App = () => {
-  
+  const ShowContent = () => {
+    return(
+      <div id="number-display">
+        1<br> 2<br> 3</br> 4</br> 5</br>
+      </div>
+      <button id="back-button">Prev Page</button>
+      <button id="next-button">Next Page</button>
+    )
+  }
 
   return (
     <div id="main">
-
-      <div d="number-display">
-        1<br> 2<br> 3</br> 4</br> 5</br>
-      </div>
+      </ShowContent>
+    
+      <div></div>
+    
     </div>
   )
 }
